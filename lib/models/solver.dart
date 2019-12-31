e5614080c70844b76751b51dc240a11ee4d4e99a
Renato Soma <renatoys08@gmail.com>
2019-12-30 23:03:56 -0300

    Begin color flooding
    Begin backtrack logic to flood the board with a selected color.

--- lib/models/solver.dart
+++ lib/models/solver.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+enum BoardColors { red, green, blue, purple, orange }
+
 class Solver {
   var _matrix;
   var _boardSize;
@@ -9,13 +11,41 @@ class Solver {
   }
 
   void generateBoard() {
-    _matrix = List(_boardSize);
-    for (int i = 0; i < _boardSize; i++) {
-      _matrix[i] = [for (var i = 0; i < _boardSize; i++) Random().nextInt(_boardSize)];
-    }
+    // _matrix = [[1,1,1],[1,1,1],[1,1,1]];
+    _matrix = [[BoardColors.green, BoardColors.green, BoardColors.green],
+               [BoardColors.red, BoardColors.red, BoardColors.green],
+               [BoardColors.blue, BoardColors.green, BoardColors.green]];
+  //   _matrix = List(_boardSize);
+  //   for (int i = 0; i < _boardSize; i++) {
+  //     _matrix[i] = [
+  //       for (var i = 0; i < _boardSize; i++) BoardColors.values[Random().nextInt(_boardSize)]
+  //     ];
+  //   }
   }
 
   void printMatrix() {
     print(_matrix);
+    print("change color");
+    modifyColor(BoardColors.blue, _matrix[0][0], 0, 0);
+    print(_matrix);
   }
+
+//bug -> not backtracking correctly
+  void modifyColor(BoardColors newColor, BoardColors currColor, int row, int col) {
+    if (row == col && row == _boardSize) {
+      _matrix[row][col] = newColor;
+      return;
+    }
+
+    if (row + 1 < _boardSize && currColor == _matrix[row+1][col]) {
+      modifyColor(newColor, currColor, row + 1, col);
+    }
+
+    if (col + 1 < _boardSize && currColor == _matrix[row][col+1]) {
+      modifyColor(newColor, currColor, row, col + 1);
+    }
+
+    _matrix[row][col] = newColor;
+  }
+
 }
