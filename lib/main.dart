5fc60b8ad4c4c20df7c58f183b0293cb050a7048
Renato Soma <renatoys08@gmail.com>
2020-01-05 20:46:10 -0300

    Start board UI
    Begin to implement a simple board using rows and columns.
    Need to implement a listener to each cell as a next step,
    before moving on to refactoring the board creationg and
    removing the hard coded values.

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,86 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    Solver solv = Solver(3);
-    solv.generateBoard();
-    print("Print initial board:");
-    solv.printMatrix();
-    print("Call solver");
-    solv.debugTrivialBoard();
+    // Solver solv = Solver(3);
+    // solv.generateBoard();
+    // print("Print initial board:");
+    // solv.printMatrix();
+    // print("Call solver");
+    // solv.debugTrivialBoard();
 
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("ColorGame"),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: Column( //for now, keep it as a single column with rows inside
-        // children: <Widget>[_coloredRowWidgetBuilder()],
-        children: <Widget>[Container(color: Colors.greenAccent,)],
-      ),
-    ));
+            appBar: AppBar(
+              title: Text("ColorGame"),
+              centerTitle: true,
+              backgroundColor: Colors.deepPurple,
+            ),
+            body: LayoutBuilder(
+              builder: (context, constraints) {
+                var height = constraints.maxHeight;
+                var width = constraints.maxWidth;
+                print(constraints.maxWidth);
+                print(constraints.maxHeight);
+                return _createBoard(height, width);
+              },
+            )));
   }
 
-  //nested columns, creating a non scrollable grid
-  Widget _coloredRowWidgetBuilder() {
-    return Row(
+  Widget _createBoard(var _boardHeigth, var _boardWidth) {
+    return Column(
+      //for now, keep it as a single column with rows inside
       children: <Widget>[
-        _singleColoredCellBuilder(),
-        _singleColoredCellBuilder(),
+        _coloredRowWidgetBuilder(_boardHeigth / 10, _boardWidth),
       ],
     );
   }
 
-  Widget _singleColoredCellBuilder() {
-    print("im here");
-    return Column(
-      children: <Widget>[Container(color: Colors.deepPurple,)],
+  //nested columns, creating a non scrollable grid
+  Widget _coloredRowWidgetBuilder(var _rowHeight, _rowWidth) {
+    print("Row heigth: $_rowHeight");
+    print("Row width: $_rowWidth");
+    return Container(
+      child: Padding(
+        //10 px from both margins
+        padding: EdgeInsets.only(right: 10, left: 10),
+        child: Row(
+          children: <Widget>[
+            //force 8 elements to render, need to refactor this later on
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+            _singleColoredCellBuilder(_rowHeight, _rowWidth / 10),
+          ],
+        ),
+      ),
+      height: _rowHeight,
+      width: _rowWidth,
     );
+  }
+
+  Widget _singleColoredCellBuilder(var _cellHeight, var _cellWidth) {
+    print("cell heigth: $_cellHeight");
+    print("cell width: $_cellWidth");
+    print("im here");
+
+    //use the smallest size, for now
+    var _size = _cellHeight > _cellWidth ? _cellWidth : _cellHeight;
+    //cells with 5 px of distance from each other
+    return Padding(
+        padding: EdgeInsets.only(left: 5),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+                width: _size,
+                height: _size,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.deepPurpleAccent),
+                )),
+          ],
+        ));
   }
 }
