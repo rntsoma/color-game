e410078cfe8c948a2f1d926ba25b421371a0d709
Renato Soma <renatoys08@gmail.com>
2020-01-07 20:51:21 -0300

    Generalize code, create simple grid
    Still need to create a nested container where the board uses 4/5 of the
    screen space and the controlling buttons use 1/5 of it.

--- lib/main.dart
+++ lib/main.dart
@@ -25,81 +25,76 @@ class _HomeState extends State<Home> {
               centerTitle: true,
               backgroundColor: Colors.deepPurple,
             ),
-            body: LayoutBuilder(
-              builder: (context, constraints) {
-                var height = constraints.maxHeight;
-                var width = constraints.maxWidth;
-                print(constraints.maxWidth);
-                print(constraints.maxHeight);
-                return _createBoard(height, width);
-              },
-            )));
+            body: Padding(
+                padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    const lines = 10;
+                    var height = constraints.maxHeight;
+                    var width = constraints.maxWidth;
+                    var smallestDimension = height > width ? width : height;
+                    var _cellSize = smallestDimension/lines;
+                    print(constraints.maxWidth);
+                    print(constraints.maxHeight);
+                    return _createBoard(smallestDimension, _cellSize, lines);
+                  },
+                ))));
   }
 
-  Widget _createBoard(var _boardHeigth, var _boardWidth) {
-    List<Widget> _createRowList(var height, var width) {
+  Widget _createBoard(var _boardSize, var _cellSize, var _rowCellCount) {
+    List<Widget> _createRowList() {
       List<Widget> rowList = [];
-      for(int i=0; i<9; i++){
-        rowList.add(_coloredRowWidgetBuilder(height, width));
+      for(int i=0; i<_rowCellCount; i++){
+        rowList.add(_coloredRowWidgetBuilder(_boardSize, _cellSize, _rowCellCount));
       }
       return rowList;
     }
 
-    return Padding(
-      padding: EdgeInsets.only(left: 5, right: 5),
-      child: Column(
+    return Column(
         //for now, keep it as a single column with rows inside
-        children: <Widget>[
-          _coloredRowWidgetBuilder(_boardHeigth / 10, _boardWidth - 10),
-      ),
-    );
+        children: _createRowList()
+        );
   }
 
   //nested columns, creating a non scrollable grid
-  Widget _coloredRowWidgetBuilder(var _rowHeight, _rowWidth) {
-    print("Row heigth: $_rowHeight");
-    print("Row width: $_rowWidth");
+  Widget _coloredRowWidgetBuilder(var _rowWidth, var _cellSize, var _cellCount) {
+    bool _flipColor = false;
 
-    List<Widget> _createRowList(var height, var width) {
+    MaterialColor _changeColor(){
+      _flipColor = !_flipColor;
+      if (_flipColor) {
+        return Colors.deepPurple;
+     }
+     return Colors.green;
+    }
+
+    List<Widget> _createRowList() {
       List<Widget> rowList = [];
-      for(int i=0; i<9; i++){
-        rowList.add(_singleColoredCellBuilder(height, width));
+      for (int i = 0; i < _cellCount; i++) {
+        rowList.add(_singleColoredCellBuilder(_cellSize, _changeColor()));
       }
       return rowList;
     }
 
     return Container(
-      child: Padding(
-        //10 px from both margins
-        padding: EdgeInsets.only(right: 0, left: 0),
-        child: Row(
-          children: _createRowList(_rowHeight, _rowWidth/10)
-        ),
-      ),
-      height: _rowHeight,
+      child: Row(children: _createRowList()),
+      height: _cellSize,
       width: _rowWidth,
     );
   }
 
-  Widget _singleColoredCellBuilder(var _cellHeight, var _cellWidth) {
-    print("cell heigth: $_cellHeight");
-    print("cell width: $_cellWidth");
-    print("im here");
-
-    //use the smallest size, for now
-    var _size = _cellHeight > _cellWidth ? _cellWidth : _cellHeight;
-    //cells with 5 px of distance from each other
-    return Padding(
-        padding: EdgeInsets.only(left: 1),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-                width: _size,
-                height: _size,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(color: Colors.deepPurpleAccent),
-                )),
-          ],
-        ));
+  Widget _singleColoredCellBuilder(var _cellSize, MaterialColor color) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+            width: _cellSize,
+            height: _cellSize,
+            child: DecoratedBox(
+              decoration: BoxDecoration(color: color,
+              border: Border.all(width: 1.0, color: Colors.white)
+              ),
+            )),
+      ],
+    );
   }
 }
